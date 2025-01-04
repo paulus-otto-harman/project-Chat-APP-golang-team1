4ecd9dc71b0e9f829bef48c46a488e19a4eda495
Paulus Otto Harman <paulus.otto.harman@gmail.com>
2025-01-04 10:20:20 +0700

    refactor: update user seeder

--- auth-service/repository/otp.go
+++ auth-service/repository/otp.go
@@ -16,7 +16,7 @@ func NewOtpRepository(db *gorm.DB, log *zap.Logger) *OtpRepository {
 }
 
 func (repo *OtpRepository) Create(otp *model.Otp) error {
-	return repo.db.Create(otp).Error
+	return repo.db.Create(&otp).Error
 }
 
 func (repo *OtpRepository) Get(criteria model.Otp) (model.Otp, error) {
